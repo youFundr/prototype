baea3e5b6f1250f31ed776ca1c6b7e312714b720
Andrew Knox <andrewknox@silentk.dev>
2022-06-17 13:08:58 -0700

    updating smart contract address and abi

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const youfundrAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const youfundrAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 export const youfundrABI = [
   {
     anonymous: false,
@@ -92,7 +92,7 @@ export const youfundrABI = [
       },
       {
         internalType: "uint256",
-        name: "duration",
+        name: "deadline",
         type: "uint256",
       },
       {
